a460831e58dcb2593a62d5363b8c98540788233f
gavinfarrell <gavinfarrell97@gmail.com>
2025-10-14 18:20:20 +0100

    updates news and events

--- js/news_items.js
+++ js/news_items.js
@@ -1,16 +1,9 @@
 const newsItems = [
   {
-    title: "ELIXIR Ireland Welcomes New Partner Institution",
-    date: "2024-03-15",
-    summary: "We are thrilled to announce that University College Cork has joined the ELIXIR Ireland consortium, strengthening our national infrastructure for life science data.",
-    image: "https://via.placeholder.com/400x250/4A90E2/FFFFFF?text=New+Partner",
-    link: "#"
-  },
-  {
-    title: "International Collaboration on Data Standards",
-    date: "2023-06-10",
-    summary: "ELIXIR Ireland participates in a new international initiative to develop common data standards for life sciences research.",
-    image: "https://via.placeholder.com/400x250/E67E22/FFFFFF?text=Data+Standards",
+    title: "ELIXIR Ireland Welcomes New Head of Node",
+    date: "2025-01-05",
+    summary: "ELIXIR Ireland welcomes its new Head of Node Aedín Culhane (UL) who will lead the Node after the role handover from Denis Shields (UCD). We thank Denis for his years of stewardship and operationalisation of ELIXIR Ireland.",
+    image: "images/ul.jpeg",
     link: "#"
   }
 ];
