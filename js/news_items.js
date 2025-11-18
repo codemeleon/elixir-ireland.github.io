537a53d0e5127eebd3de820f098032540fc8ca42
gavinfarrell <gavinfarrell97@gmail.com>
2025-11-18 23:26:26 +0100

    adds deputy hon news

--- js/news_items.js
+++ js/news_items.js
@@ -7,6 +7,13 @@ const newsItems = [
     link: "https://forms.gle/x4cwk9GjZBb1E1YR6"
   },
   {
+    title: "ELIXIR Ireland Welcomes New Deputy Heads of Node",
+    date: "2025-11-11",
+    summary: "ELIXIR Ireland is pleased to welcome two new Deputy Heads of Node: Laura Whelan (RCSI) and Pilib Ó Broin (University of Galway). We look forward to their contributions in supporting the growth and development of ELIXIR Ireland.",
+    image: "images/deputyhon.png",
+    link: "#"
+  },
+  {
     title: "ELIXIR Ireland Welcomes New Head of Node",
     date: "2025-06-01",
     summary: "ELIXIR Ireland welcomes its new Head of Node Aedín Culhane (UL) who will lead the Node after the role handover from Denis Shields (UCD). We thank Denis for his years of stewardship and operationalisation of ELIXIR Ireland.",
