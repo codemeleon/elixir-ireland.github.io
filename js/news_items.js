13bb00979914211171c7576969ed3428157d657e
gavinfarrell <gavinfarrell97@gmail.com>
2025-11-18 22:44:57 +0100

    Survey added

--- js/news_items.js
+++ js/news_items.js
@@ -1,5 +1,12 @@
 const newsItems = [
   {
+    title: "Survey: Landscaping Irish Life Science Data Research Infrastructure Needs",
+    date: "2025-01-15",
+    summary: "ELIXIR Ireland and VIBE are conducting a national landscape analysis of Irish life science data requirements. Your input will shape a strategic roadmap for Ireland's data ecosystem, addressing computational tools, research computing, data management, and training needs. Complete by Nov 30, 2025 (~15 mins).",
+    image: "images/survey.jpg",
+    link: "https://forms.gle/x4cwk9GjZBb1E1YR6"
+  },
+  {
     title: "ELIXIR Ireland Welcomes New Head of Node",
     date: "2025-01-05",
     summary: "ELIXIR Ireland welcomes its new Head of Node Aedín Culhane (UL) who will lead the Node after the role handover from Denis Shields (UCD). We thank Denis for his years of stewardship and operationalisation of ELIXIR Ireland.",
